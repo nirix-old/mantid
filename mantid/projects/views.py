d5456e1a879946a87dfa3277c9656a9098f0b782
Jack Polgar <jack@polgar.id.au>
2013-10-28 13:07:20 +1100

    Projects are now listed on the index page.

--- mantid/projects/views.py
+++ mantid/projects/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from mantid.projects.models import Project
+
 def index(request):
-    template = loader.get_template('projects/index.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
+  projects = Project.objects.order_by('name')
+  template = loader.get_template('projects/index.html')
+  context = RequestContext(request, {
+    'projects': projects
+  })
+  return HttpResponse(template.render(context))
